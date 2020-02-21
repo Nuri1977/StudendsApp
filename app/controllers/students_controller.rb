5ea1b36a7e655e57891e32c71f2a2af3b0e7e2b7
nuri1977 <nurilacka@gmail.com>
2020-02-21 19:35:10 +0100

    Create action added, errors message option in entry form

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,20 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "You have successfully signed up"
+      redirect_to students_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { minimum:3, maximum: 50 }
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i       
   validates :email, presence: true, 
